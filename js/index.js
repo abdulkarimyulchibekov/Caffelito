54d1f7c4789558f1f8dc7202f9d1e471f1333219
abdulkarimyulchibekov <abdulkarimyulchibekov@gamil.com>
2026-06-05 17:46:12 +0500

    Adding language translation buttons

--- js/index.js
+++ js/index.js
@@ -2,15 +2,36 @@ let translations;
 console.log(45);
 
 fetch("./js/translations.json")
-  .then(res => res.json())
-  .then(data => {
-    translations = data;
-    setLang("rus"); // default
-  });
+.then(res => res.json())
+.then(data => {
+  translations = data;
+  setLanguage("rus"); // default
+});
 
-function setLang(lang) {
+const buttons = document.querySelectorAll(".lang-btn");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    
+    buttons.forEach(btn => {
+      btn.classList.remove("active");
+    });
+    
+    button.classList.add("active");
+    
+    const lang = button.dataset.lang;
+    
+    // Your existing translation logic
+    setLanguage(lang);
+  });
+});
+
+function setLanguage(lang) {
+  console.log("Selected language:", lang);
+  
   document.querySelectorAll("[data-i18n]").forEach(el => {
     const key = el.dataset.i18n;
     el.textContent = translations[lang][key];
   });
+  // Put your JSON translation code here
 }
